efc6856caba03d8b7a3bed20331e131390b3e5ac
rashmi171103 <104857014+rashmi171103@users.noreply.github.com>
2022-09-20 19:09:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,25 @@
 function add(){
     //row1
-// console.log('add')
-let c0 = document.getElementById('col0').value;
-let c1= document.getElementById('col1').value;
-// console.log(col0);
-let c2 = document.getElementById('col2').value;
-var row1 = Number(c0)+Number(c1)+Number(c2);
-document.getElementById('rowsum1').value = row1;
-// console.log(row1);
-
-//row 2--------------------
-
-let c3 = document.getElementById('col3').value;
-let c4= document.getElementById('col4').value;
-let c5 = document.getElementById('col5').value;
-var row2 = Number(c3)+Number(c4)+Number(c5);
-document.getElementById('rowsum2').value = row2;
-console.log(row2);
-
-//row 3---------------------------
-
-let c6 = document.getElementById('col6').value;
-let c7= document.getElementById('col7').value;
-let c8 = document.getElementById('col8').value;
-var row3 = Number(c6)+Number(c7)+Number(c8);
-document.getElementById('rowsum3').value = row3;
-console.log(row2);
-
-// column1 ------------------------------------
-
-let r1 = document.getElementById('col0').value;
-let r2= document.getElementById('col3').value;
-let r3 = document.getElementById('col6').value;
-var col1 = Number(r1)+Number(r2)+Number(r3);
-document.getElementById('colsum1').value = col1;
-
-// column 2---------------------
-let r4 = document.getElementById('col1').value;
-let r5= document.getElementById('col4').value;
-let r6 = document.getElementById('col7').value;
-var col2 = Number(r4)+Number(r5)+Number(r6);
-document.getElementById('colsum2').value = col2;
-
-//column 3--------------------
-
-let r7 = document.getElementById('col2').value;
-let r8= document.getElementById('col5').value;
-let r9 = document.getElementById('col8').value;
-var col3 = Number(r7)+Number(r8)+Number(r9);
-document.getElementById('colsum3').value = col3;
+    document.getElementById('rowsum1').value = getsumofthreecells('col0','col1','col2');
+    document.getElementById('rowsum2').value = getsumofthreecells('col3','col4','col5');
+    document.getElementById('rowsum3').value = getsumofthreecells('col6','col7','col8');
+    document.getElementById('colsum1').value = getsumofthreecells('col0','col3','col6');
+    document.getElementById('colsum2').value = getsumofthreecells('col1','col4','col7');
+    document.getElementById('colsum3').value = getsumofthreecells('col2','col5','col8');
 }
+
 
 function remove(){
     const inputs = document.querySelectorAll('#col0, #col1, #col2, #col3, #col4, #col5, #col6, #col7, #col8, #colsum1, #colsum2, #colsum3, #rowsum1, #rowsum2, #rowsum3' );
     inputs.forEach(input => {
         input.value = " ";
     })
+}
+
+function getsumofthreecells(cell1, cell2, cell3){
+    let r1 = document.getElementById(cell1).value;
+    let r2= document.getElementById(cell2).value;
+    let r3 = document.getElementById(cell3).value;
+    var val = Number(r1)+Number(r2)+Number(r3);
+    return val;
 }
